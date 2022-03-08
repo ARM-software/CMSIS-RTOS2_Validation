abe29f2083c4d166e274e4a848e6e40eeaa93e8f
Vladimir Marchenko <Vladimir.Marchenko@arm.com>
2022-03-08 12:02:02 +0100

    Updated documentation to doxygen 1.9.2 and new layout

--- Doxygen/_DoxyTemplates/_cp_search.bat
+++
@@ -1,5 +0,0 @@
-copy /Y search.css ..\General\html\search\. 
-copy /Y search.css ..\CORE\html\search\. 
-copy /Y search.css ..\DSP\html\search\. 
-copy /Y search.css ..\SVD\html\search\. 
-copy /Y search.css ..\RTOS\html\search\. 
