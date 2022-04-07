d8ecd74f03b7becd45965813f731f7178130276d
Vladimir Umek <vladimir.umek@arm.com>
2022-04-07 13:29:22 +0200

    Update test report output
    - By default, output is via private printf implementation that uses stdout_putchar(ch) as output channel
    - By default, end of line consists of \r\n

--- Source/tf_report.c
+++ Source/tf_report.c
@@ -21,24 +21,52 @@
 
 #include "tf_report.h"
 
+/*
+  Select test report print output
+  0: Standard C library STDOUT
+  1: Private printf implementation calling extern function stdout_putchar(ch)
+*/
+#ifndef TF_OUTPUT
+#define TF_OUTPUT           1
+#endif
+
+/*
+  Select print statement EOL style
+  0: End Of Line: \n
+  1: End Of Line: \r\n
+*/
+#ifndef TF_OUTPUT_CRLF
+#define TF_OUTPUT_CRLF      1
+#endif
+
 TEST_REPORT test_report;
 static AS_STAT     current_assertions;   /* Current test case assertions statistics */
 #define TAS (&test_report.assertions)    /* Total assertions                        */
 #define CAS (&current_assertions)        /* Current assertions                      */
 
-#ifdef DISABLE_SEMIHOSTING
-#if defined (__CC_ARM)
-  #pragma import __use_no_semihosting
-#elif defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
-  __ASM(".global __use_no_semihosting");
-#endif
-#define PRINT(x)
-#define FLUSH()
-void _sys_exit(int return_code) {}
+/* Select test report print output */
+#if (TF_OUTPUT == 0)
+  /* Use standard C library functions */
+  #define PRINT(x) print_stdio x
+  #define FLUSH()  flush_stdio()
+#elif (TF_OUTPUT == 1)
+  /* Use static printf implementation and out_putchar() */
+  #define PRINT(x) printf_lim x
+  #define FLUSH()
 #else
-#define PRINT(x) MsgPrint x
-#define FLUSH()  MsgFlush()
-#endif // DISABLE_SEMIHOSTING
+  #error "Unknown TF_OUTPUT setting."
+#endif
+
+/* Select print statement EOL style */
+#if (TF_OUTPUT_CRLF == 0)
+  /* End Of Line: \n */
+  #define TF_EOL "\n"
+#elif (TF_OUTPUT_CRLF == 1)
+  /* End Of Line: \r\n */
+  #define TF_EOL "\r\n"
+#else
+  #error "Invalid TF_OUTPUT_CRLF setting."
+#endif
 
 static uint8_t Passed[] = "PASSED";
 static uint8_t Warning[] = "WARNING";
@@ -74,8 +102,14 @@ static bool     StatCount (TC_RES res);
 /*-----------------------------------------------------------------------------
  * Printer function prototypes
  *----------------------------------------------------------------------------*/
-static void MsgPrint (const char *msg, ...);
-static void MsgFlush (void);
+#if (TF_OUTPUT == 0)
+static void print_stdio (const char *msg, ...);
+static void flush_stdio (void);
+#elif (TF_OUTPUT == 1)
+extern int stdout_putchar (int ch);
+static int out_putchar    (int ch);
+static int printf_lim (const char *fmt, ...);
+#endif
 
 
 /*-----------------------------------------------------------------------------
@@ -119,20 +153,20 @@ bool tr_File_Init (void) {
 /*-----------------------------------------------------------------------------
  * Open test report
  *----------------------------------------------------------------------------*/
-#if (PRINT_XML_REPORT==1)
+#if (PRINT_XML_REPORT == 1)
 bool tr_File_Open (const char *title, const char *date, const char *time, const char *fn) {
-  PRINT(("<?xml version=\"1.0\"?>\n"));
-  PRINT(("<?xml-stylesheet href=\"TR_Style.xsl\" type=\"text/xsl\" ?>\n"));
-  PRINT(("<report>\n"));
-  PRINT(("<test>\n"));
-  PRINT(("<title>%s</title>\n", title));
-  PRINT(("<date>%s</date>\n",   date));
-  PRINT(("<time>%s</time>\n",   time));
-  PRINT(("<file>%s</file>\n",   fn));
-  PRINT(("<test_cases>\n"));
+  PRINT(("<?xml version=\"1.0\"?>%s", TF_EOL));
+  PRINT(("<?xml-stylesheet href=\"TR_Style.xsl\" type=\"text/xsl\" ?>%s", TF_EOL));
+  PRINT(("<report>%s", TF_EOL));
+  PRINT(("<test>%s", TF_EOL));
+  PRINT(("<title>%s</title>%s", title, TF_EOL));
+  PRINT(("<date>%s</date>%s",   date,  TF_EOL));
+  PRINT(("<time>%s</time>%s",   time,  TF_EOL));
+  PRINT(("<file>%s</file>%s",   fn,    TF_EOL));
+  PRINT(("<test_cases>%s", TF_EOL));
 #else
 bool tr_File_Open (const char *title, const char *date, const char *time, const char __attribute__((unused)) *fn) {
-  PRINT(("%s   %s   %s \n\n", title, date, time));
+  PRINT(("%s   %s   %s %s%s", title, date, time, TF_EOL));
 #endif
   FLUSH();
 
@@ -145,13 +179,13 @@ bool tr_File_Open (const char *title, const char *date, const char *time, const 
  *----------------------------------------------------------------------------*/
 bool tc_File_Open (uint32_t num, const char *fn) {
   tc_Init ();
-#if (PRINT_XML_REPORT==1)
-  PRINT(("<tc>\n"));
-  PRINT(("<no>%d</no>\n",     num));
-  PRINT(("<func>%s</func>\n", fn));
+#if (PRINT_XML_REPORT == 1)
+  PRINT(("<tc>%s", TF_EOL));
+  PRINT(("<no>%d</no>%s",     num, TF_EOL));
+  PRINT(("<func>%s</func>%s", fn,  TF_EOL));
   PRINT(("<req></req>"));
   PRINT(("<meth></meth>"));
-  PRINT(("<dbgi>\n"));
+  PRINT(("<dbgi>%s", TF_EOL));
 #else
   PRINT(("TEST %02d: %-32s ", num, fn));
 #endif
@@ -166,15 +200,15 @@ bool tc_File_Open (uint32_t num, const char *fn) {
  *----------------------------------------------------------------------------*/
 bool tc_File_Close (void) {
   uint8_t *res = tc_Eval();
-#if (PRINT_XML_REPORT==1)
-  PRINT(("</dbgi>\n"));
-  PRINT(("<res>%s</res>\n", res));
-  PRINT(("</tc>\n"));
+#if (PRINT_XML_REPORT == 1)
+  PRINT(("</dbgi>%s", TF_EOL));
+  PRINT(("<res>%s</res>%s", res, TF_EOL));
+  PRINT(("</tc>%s", TF_EOL));
 #else
   if ((res==Passed)||(res==NotExe))
-    PRINT(("%s\n", res));
+    PRINT(("%s%s", res, TF_EOL));
   else
-    PRINT(("\n"));
+    PRINT(("%s", TF_EOL));
 #endif
   FLUSH();
 
@@ -186,26 +220,27 @@ bool tc_File_Close (void) {
  * Close test report
  *----------------------------------------------------------------------------*/
 bool tr_File_Close (void) {
-#if (PRINT_XML_REPORT==1)
-  PRINT(("</test_cases>\n"));
-  PRINT(("<summary>\n"));
-  PRINT(("<tcnt>%d</tcnt>\n", test_report.tests));
-  PRINT(("<exec>%d</exec>\n", test_report.executed));
-  PRINT(("<pass>%d</pass>\n", test_report.passed));
-  PRINT(("<fail>%d</fail>\n", test_report.failed));
-  PRINT(("<warn>%d</warn>\n", test_report.warnings));
-  PRINT(("<tres>%s</tres>\n", tr_Eval()));
-  PRINT(("</summary>\n"));
-  PRINT(("</test>\n"));
-  PRINT(("</report>\n"));
+#if (PRINT_XML_REPORT == 1)
+  PRINT(("</test_cases>%s", TF_EOL));
+  PRINT(("<summary>%s", TF_EOL));
+  PRINT(("<tcnt>%d</tcnt>%s", test_report.tests,    TF_EOL));
+  PRINT(("<exec>%d</exec>%s", test_report.executed, TF_EOL));
+  PRINT(("<pass>%d</pass>%s", test_report.passed,   TF_EOL));
+  PRINT(("<fail>%d</fail>%s", test_report.failed,   TF_EOL));
+  PRINT(("<warn>%d</warn>%s", test_report.warnings, TF_EOL));
+  PRINT(("<tres>%s</tres>%s", tr_Eval(),            TF_EOL));
+  PRINT(("</summary>%s", TF_EOL));
+  PRINT(("</test>%s", TF_EOL));
+  PRINT(("</report>%s", TF_EOL));
 #else
-  PRINT(("\nTest Summary: %d Tests, %d Executed, %d Passed, %d Failed, %d Warnings.\n",
+  PRINT(("\nTest Summary: %d Tests, %d Executed, %d Passed, %d Failed, %d Warnings.%s",
          test_report.tests,
          test_report.executed,
          test_report.passed,
          test_report.failed,
-         test_report.warnings));
-  PRINT(("Test Result: %s\n", tr_Eval()));
+         test_report.warnings,
+         TF_EOL));
+  PRINT(("Test Result: %s%s", tr_Eval(), TF_EOL));
 #endif
   FLUSH();
 
@@ -224,13 +259,13 @@ bool As_File_Result (TC_RES res) {
 /*-----------------------------------------------------------------------------
  * Set debug information state
  *----------------------------------------------------------------------------*/
-#if (PRINT_XML_REPORT==1)
+#if (PRINT_XML_REPORT == 1)
 bool As_File_Dbgi (TC_RES __attribute__((unused)) res, const char *fn, uint32_t ln, char *desc) {
-  PRINT(("<detail>\n"));
-  if (desc!=NULL) PRINT(("<desc>%s</desc>\n", desc));
-  PRINT(("<module>%s</module>\n", fn));
-  PRINT(("<line>%d</line>\n", ln));
-  PRINT(("</detail>\n"));
+  PRINT(("<detail>%s", TF_EOL));
+  if (desc!=NULL) PRINT(("<desc>%s</desc>%s", desc, TF_EOL));
+  PRINT(("<module>%s</module>%s", fn, TF_EOL));
+  PRINT(("<line>%d</line>%s", ln, TF_EOL));
+  PRINT(("</detail>%s", TF_EOL));
 #else
 bool As_File_Dbgi (TC_RES res, const char *fn, uint32_t ln, char *desc) {
   PRINT(("\n  %s (%d)", fn, ln));
@@ -403,28 +438,256 @@ uint32_t __assert_true (const char *fn, uint32_t ln, uint32_t cond) {
   return (cond);
 }
 
-#ifndef DISABLE_SEMIHOSTING
-/*-----------------------------------------------------------------------------
- *       MsgFlush:  Flush the standard output
- *----------------------------------------------------------------------------*/
-void MsgFlush(void) {
-  fflush(stdout);
-}
 
-#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-#endif
+#if (TF_OUTPUT == 0)
 /*-----------------------------------------------------------------------------
- *       MsgPrint:  Print a message to the standard output
+ *       print_stdio:  Print a message to the standard output
  *----------------------------------------------------------------------------*/
-void MsgPrint (const char *msg, ...) {
+void print_stdio (const char *msg, ...) {
   va_list args;
   va_start(args, msg);
   vprintf(msg, args);
   va_end(args);
 }
-#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
-#pragma clang diagnostic pop
-#endif
-#endif // DISABLE_SEMIHOSTING
+
+/*-----------------------------------------------------------------------------
+ *       flush_stdio:  Flush the standard output
+ *----------------------------------------------------------------------------*/
+void flush_stdio (void) {
+  fflush(stdout);
+}
+
+#elif (TF_OUTPUT == 1)
+
+/**
+  Reverse string str
+*/
+static void rev (char *str) {
+  unsigned int i, j;
+  char c;
+
+  i = 0;
+  j = strlen(str) - 1;
+
+  while (i < j) {
+    c = str[i];
+
+    str[i] = str[j];
+    str[j] = c;
+    
+    i++;
+    j--;
+  }
+}
+
+/**
+  Convert integer n to string and store it to str buffer
+*/
+static char *itoa (int n, char *str, int base) {
+  int s;
+  int i;
+
+  /* Limit to base 10 */
+  if (base != 10) {
+    return str;
+  }
+
+  /* Store sign */
+  s = n;
+
+  if (n < 0) {
+    /* Convert to positive value */
+    n = -n;
+  }
+
+  /* Convert to digits in reverse order */
+  i = 0;
+
+  do {
+    str[i] = (char)(n % 10) + '0';
+    i++;
+
+    n = n / 10;
+  }
+  while (n > 0);
+  
+  /* Add sign */
+  if (s < 0) {
+    str[i++] = '-';
+  }
+
+  /* Terminate the string */
+  str[i] = '\0';
+
+  /* Reverse value before we return */
+  rev(str);
+
+  return str;
+}
+
+/**
+  Convert string to integer, return pointer to first character after number in end
+*/
+static int atoi_ex (const char *str, const char **end) {
+  int s;
+  int i;
+  int r;
+
+  /* Init sign, result and index */
+  s = 1;
+  r = 0;
+  i = 0;
+
+  if (str[i] == '-') {
+    /* Negative sign */
+    s = -1;
+    i++;
+  }
+  
+  while ((str[i] >= '0') && (str[i] <= '9')) {
+    r  = r * 10;
+    r += str[i] - '0';
+
+    i++;
+  }
+
+  /* Add correct sign */
+  r *= s;
+  
+  *end = &str[i];
+
+  return (r);
+}
+
+
+/*
+  Limited vfprintf function
+
+  Supports:
+  - flags tags: %-
+  - width field: supported, * (asterisk) not supported
+  - format tags: %d, %s
+*/
+static int vprintf_lim (char const *fmt, va_list arg) {
+  char  ch, *pch;
+  char *a_string;
+  int   a_int;
+  int   len;
+  unsigned int slen;
+  int   m, p;
+
+  /* Stacked vfprintf buffer */
+  char  buf[512];
+
+  len = 0;
+
+  while (*fmt) {
+    ch = *fmt++;
+
+    if (ch == '%') {
+      /* Process a format specifier */
+      ch = *fmt++;
+      m = 0;
+      p = 0;
+
+      /* Check flags */
+      if (ch == '-') {
+        /* %-xxX: left-justify within given width */
+        m  = atoi_ex (fmt, &fmt);
+        ch = *fmt++;
+      }
+      else if ((ch >= '0') && (ch <= '9')) {
+        /* %0xX: prepend zero or space, ensure minimum num of characters to be printed */
+        p = atoi_ex (fmt, &fmt);
+        ch = *fmt++;
+      }
+
+      /* Print argument value */
+      switch (ch) {
+        case 'd': /* %d: print an integer */
+          a_int = va_arg(arg, int);
+
+          /* Convert value */
+          itoa (a_int, buf, 10);
+
+          slen = strlen(buf);
+
+          /* Left-pad the number with spaces */
+          p -= slen;
+          while (p > 0) {
+            out_putchar(' ');
+            len++;
+            p--;
+          }
+
+          /* Send to output */
+          
+          pch = buf;
+          while (*pch) {
+            out_putchar(*pch++);
+          }
+          len += slen;
+
+          m -= slen;
+
+          while (m > 0) {
+            out_putchar(' ');
+            len++;
+            m--;
+          }
+          break;
+
+        case 's': /* %s: print a string */
+          a_string = va_arg(arg, char *);
+
+          slen = strlen (a_string);
+
+          /* Send to output */
+          pch = a_string;
+          while (*pch) {
+            out_putchar(*pch++);
+          }
+          len += slen;
+
+          m -= slen;
+
+          while (m > 0) {
+            out_putchar(' ');
+            len++;
+            m--;
+          }
+          break;
+
+        default:
+          /* Everything else is unsupported */
+          out_putchar(ch);
+          len++;
+          break;
+      }
+    }
+    else {
+      /* Send directly to output */
+      out_putchar(ch);
+      len++;
+    }
+  }
+
+  return len;
+}
+
+
+static int printf_lim (const char *fmt, ...) {
+  va_list arg;
+  int len;
+
+  va_start(arg, fmt);
+  len = vprintf_lim (fmt, arg);
+  va_end(arg);
+
+  return len;
+}
+
+static int out_putchar (int ch) {
+  return stdout_putchar(ch);
+}
+#endif /* TF_OUTPUT */
