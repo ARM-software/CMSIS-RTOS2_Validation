4b56ce81a6dec240d8cb56031eeba693935723a9
Vladimir Umek <vladimir.umek@arm.com>
2022-08-19 12:22:08 +0200

    Cleanup test report output implementation

--- Include/tf_report.h
+++ Include/tf_report.h
@@ -19,9 +19,7 @@
 #ifndef TF_REPORT_H__
 #define TF_REPORT_H__
 
-#include <stdbool.h>
-#include <stdarg.h>
-#include "tf_assert.h"
+#include <stdint.h>
 
 /*-----------------------------------------------------------------------------
  * Test Report Configuration
@@ -40,16 +38,12 @@
   Set the buffer size for assertions results buffer
 */
 #ifndef BUFFER_ASSERTIONS
-#define BUFFER_ASSERTIONS           128
+#define BUFFER_ASSERTIONS           16
 #endif
 
 /*-----------------------------------------------------------------------------
  * Test report global definitions
  *----------------------------------------------------------------------------*/
-#define REP_TC_FAIL 0
-#define REP_TC_WARN 1
-#define REP_TC_PASS 2
-#define REP_TC_NOEX 3
 
 /* Test case result definition */
 typedef enum {
@@ -61,60 +55,35 @@ typedef enum {
 
 /* Assertion result info */
 typedef struct {
-  const char    *module;                  /* Module name                        */
-  uint32_t       line;                    /* Assertion line                     */
+  const char    *result;            /* Assertion result */
+  const char    *module;            /* Module name      */
+  uint32_t       line;              /* Assertion line   */
 } AS_INFO;
-
-/* Test case callback interface definition */
-typedef struct {
-  bool (* Result) (TC_RES res);
-  bool (* Dbgi)   (TC_RES res, const char *fn, uint32_t ln, char *desc);
-} TC_ITF;
-
-/* Assert interface to the report */
-extern TC_ITF tcitf;
-
-/* Assertion result buffer */
-typedef struct {
-  AS_INFO passed[BUFFER_ASSERTIONS];
-  AS_INFO failed[BUFFER_ASSERTIONS];
-  AS_INFO warnings[BUFFER_ASSERTIONS];
-} AS_T_INFO;
 
 /* Assertion statistics */
 typedef struct {
-  uint32_t passed;           /* Total assertions passed                  */
-  uint32_t failed;           /* Total assertions failed                  */
-  uint32_t warnings;         /* Total assertions warnings                */
-  AS_T_INFO info;            /* Detailed assertion info                  */
+  uint32_t passed;                  /* Assertions passed   */
+  uint32_t failed;                  /* Assertions failed   */
+  uint32_t warnings;                /* Assertions warnings */
 } AS_STAT;
 
 /* Test global statistics */
 typedef struct {
-  uint32_t  tests;           /* Total test cases count                   */
-  uint32_t  executed;        /* Total test cases executed                */
-  uint32_t  passed;          /* Total test cases passed                  */
-  uint32_t  failed;          /* Total test cases failed                  */
-  uint32_t  warnings;        /* Total test cases warnings                */
-  AS_STAT   assertions;      /* Total assertions statistics              */
+  uint32_t  tests;                  /* Total test cases count         */
+  uint32_t  executed;               /* Total test cases executed      */
+  uint32_t  passed;                 /* Total test cases passed        */
+  uint32_t  failed;                 /* Total test cases failed        */
+  uint32_t  warnings;               /* Total test cases warnings      */
+  AS_STAT   assertions;             /* Total assertions statistics    */
+  AS_INFO   info[BUFFER_ASSERTIONS];/* Assertion failure/warning info */
 } TEST_REPORT;
 
 /* Test report interface */
-typedef struct {
-  bool (* Init)     (void);
-  bool (* Open)     (const char *title, const char *date, const char *time, const char *fn);
-  bool (* Close)    (void);
-  bool (* Open_TC)  (uint32_t num, const char *fn);
-  bool (* Close_TC) (void);
-} REPORT_ITF;
-
-/* Test report statistics */
-extern TEST_REPORT  test_report;
-
-/* Test report interface */
-extern REPORT_ITF   ritf;
-
-/* Assertions and test results */
-extern uint32_t __assert_true (const char *fn, uint32_t ln, uint32_t cond);
+int32_t TReport_Init     (void);
+int32_t TReport_Open     (const char *title, const char *date, const char *time, const char *fn);
+int32_t TReport_Close    (void);
+int32_t TReport_TestOpen (uint32_t num, const char *fn);
+int32_t TReport_TestAdd  (const char *fn, uint32_t ln, char *desc, TC_RES res);
+int32_t TReport_TestClose(void);
 
 #endif /* TF_REPORT_H__ */
